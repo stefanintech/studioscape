b19d549e03a4cf8bc38d1004d2c089dd3feb1451
Stefan <stefantaitano@gmail.com>
2023-07-23 00:34:09 -0700

    create pages and routes

---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,7 @@
+function ForgotPassword() {
+    return (
+      <h1>Forgot Password</h1>
+    )
+  }
+  
+  export default ForgotPassword
---
+++ src/pages/Offers.jsx
@@ -0,0 +1,7 @@
+function Offers() {
+    return (
+      <h1>Offers</h1>
+    )
+  }
+  
+  export default Offers
