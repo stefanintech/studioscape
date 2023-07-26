7b9a7fdf9f3f2d3ad5fb7ae1ab170bbf5a4ba6d5
Stefan <stefantaitano@gmail.com>
2023-07-26 01:05:40 -0600

    add login feature auth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import {Link, useNavigate} from 'react-router-dom'
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -20,6 +21,24 @@ function SignIn() {
         }))
     }
 
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const auth = getAuth()
+
+            const userCredential = await signInWithEmailAndPassword(auth, email, password)
+
+            if(userCredential.user){
+                navigate('/')
+            }
+        } catch (error) {
+            console.log(error)   
+        }
+
+        
+    }
+
     return (
       <>
         <div className="pageContainer">
@@ -30,7 +49,7 @@ function SignIn() {
             </header>
 
             <main>
-                <form>
+                <form onSubmit={onSubmit}>
                     <input type="email" className="emailInput" placeholder="Email" id="email" value={email} onChange={onChange} />
                     <div className="passwordInputDiv">
                         <input type={showPassword ? 'text' : 'password'} className="passwordInput" placeholder="Password" id="password" value={password} onChange={onChange} />
