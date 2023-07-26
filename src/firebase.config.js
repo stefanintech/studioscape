f2b8ca327f329bf4d1bbcabc4c4f469e6ae3965b
Stefan <stefantaitano@gmail.com>
2023-07-25 23:26:18 -0600

    create Auth for registering user

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,7 +1,9 @@
 
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFireStore } from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from "firebase/auth";
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -19,4 +21,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export const db = getFireStore()
+export const db = getFirestore()
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import {Link, useNavigate} from 'react-router-dom'
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from "firebase/auth";
+import {db} from '../firebase.config'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -21,6 +23,26 @@ function SignUp() {
         }))
     }
 
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const auth = getAuth()
+
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+            const user = userCredential.user
+
+            updateProfile(auth.currentUser, {
+                displayName: name
+            })
+
+            navigate('/')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
       <>
         <div className="pageContainer">
@@ -31,11 +53,8 @@ function SignUp() {
             </header>
 
             <main>
-                <form>
-                <input type="text" className="nameInput" placeholder="Name" id="name" value={name} onChange={onChange} />
-
-
-
+                <form onSubmit={onSubmit}>
+                    <input type="text" className="nameInput" placeholder="Name" id="name" value={name} onChange={onChange} />
                     <input type="email" className="emailInput" placeholder="Email" id="email" value={email} onChange={onChange} />
                     <div className="passwordInputDiv">
                         <input type={showPassword ? 'text' : 'password'} className="passwordInput" placeholder="Password" id="password" value={password} onChange={onChange} />
