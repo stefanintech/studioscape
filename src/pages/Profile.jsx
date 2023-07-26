3f55b7d903fd898eaa825fee4d3aa2c4602e077b
Stefan <stefantaitano@gmail.com>
2023-07-26 01:33:36 -0600

    add logout feature

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,35 @@
-
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import {getAuth} from 'firebase/auth'
+import { useNavigate, Link } from 'react-router-dom'
 
 function Profile() {
-  const [user, setUser] = useState(null)
-  
   const auth = getAuth()
-  useEffect(() => {
-    setUser(auth.currentUser)
-  }, [])  
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  })
+
+  const {name, email} = formData
+
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    auth.signOut()
+    navigate('/')
+  }
   
-  return user ? <h1>{user.displayName}</h1> : 'Not Logged In'
+ 
+  
+  return <>
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type='button' className="logOut" onClick={onLogout}>
+          Logout
+        </button>
+      </header>
+    </div>
+  </>
   }
   
   export default Profile
