2f6fb38fdb6375c4a53366eb639a9c629f2c213e
Stefan <stefantaitano@gmail.com>
2023-07-26 01:11:45 -0600

    add react toastify

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import {toast} from 'react-toastify'
 import {Link, useNavigate} from 'react-router-dom'
 import {getAuth, createUserWithEmailAndPassword, updateProfile} from "firebase/auth";
 import {setDoc, doc, serverTimestamp} from 'firebase/firestore'
@@ -46,7 +47,7 @@ function SignUp() {
 
             navigate('/')
         } catch (error) {
-            console.log(error)
+            toast.error('Hmm, check your registration because something went wrong..')
         }
     }
 
